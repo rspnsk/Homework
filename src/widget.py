ac1725afc932d4d005818617f5338a169d021097
Yuri <rspnsk@mail.ru>
2025-08-30 21:47:16 +0300

    add функция get_date

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,6 @@ import re
 from masks import get_mask_card_number
 from masks import get_mask_account
 
-s = "Visa Gold 5999414228426355"
 def mask_account_card(s: str) -> str:
     "обрабатывает информацию о картах И счетах"
     "возвращает строку с замаскированным номером карты или счета"
@@ -14,10 +13,8 @@ def mask_account_card(s: str) -> str:
     else:
         return "Некорректный ввод"
 
-print(mask_account_card(s))
 
 def get_date(data: str) -> str:
     data_1 = data[:10]
     data_2 = data_1.split('-')
     return f"{data_2[2]}.{data_2[1]}.{data_2[0]}"
-print(get_date("2024-03-11T02:26:18.671407"))
