fc0269ab31705f76d3266ba9e6f3ab2ed8543b53
Yuri <rspnsk@mail.ru>
2025-09-25 20:58:11 +0300

    изменения в файле после тестов

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Dict
+from typing import Any, Dict
 
 
 def filter_by_state(my_list_dict: list, state: str = 'EXECUTED') -> list:
@@ -12,13 +12,18 @@ def filter_by_state(my_list_dict: list, state: str = 'EXECUTED') -> list:
     return filter_my_list_dict
 
 
-def sort_by_date(my_list_dict: list, descending: bool = True) -> list:
-    '''принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
-    Функция должна возвращать новый список, отсортированный по дате (date)'''
+def get_date(transaction: Dict[str, str]) -> datetime:
+    '''Извлекаем дату из словаря и приводим её к типу datetime'''
+    return datetime.fromisoformat(transaction['date'])
 
-    def get_date(transaction: Dict[str, str]) -> datetime:
-        '''Извлекаем дату из словаря и приводим её к типу datetime'''
-        return datetime.fromisoformat(transaction['date'])
-    sort_my_list_dict = sorted(my_list_dict, key=get_date, reverse=descending)
-    return sort_my_list_dict
-# дополнительный комментарий
+
+def sort_by_date(my_list_dict: list[dict[Any, Any]], descending: bool = True) -> list[dict[Any, Any]]:
+    '''Функция сортирует список словарей по дате, по умолчанию — по убыванию'''
+
+    # Проверяем, что даты корректны
+    try:
+        # Сортируем список по дате
+        sorted_list = sorted(my_list_dict, key=get_date, reverse=descending)
+        return sorted_list
+    except ValueError:
+        return [{"error": "Некорректная дата"}]
