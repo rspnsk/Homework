5224cde9f652548bf98aa0a86dc003f4f550a9fb
Yuri <rspnsk@mail.ru>
2025-10-15 01:42:34 +0300

    в директорию src добавлены модули external_api и utils

---
+++ src/external_api.py
@@ -0,0 +1,25 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_convert_sum(transaction: dict) -> float:
+    """Функция для конвертации валюты"""
+    url = "https://api.apilayer.com/exchangerates_data/convert"
+
+    payload = {
+        "amount": transaction["operationAmount"]["amount"],
+        "from": transaction["operationAmount"]["currency"]["code"],
+        "to": "RUB"
+    }
+
+    api_key = os.getenv("API_KEY")
+    headers = {"apikey": api_key}
+
+    response = requests.get(url, headers=headers, params=payload)
+
+    result = response.json()
+    return result["result"]
