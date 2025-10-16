cae7ab8ab5caa080f09a4f6ba77e1ce4909cd92c
Yuri <rspnsk@mail.ru>
2025-10-17 00:18:34 +0300

    в функции get_convert_sum поменял тип возвращаемых данных на float и изменил функцию, что бы она обращалась к API, только если валюта не рубли.

--- src/external_api.py
+++ src/external_api.py
@@ -24,21 +24,3 @@ def get_convert_sum(transaction: dict) -> float:
         response = requests.get(url, headers=headers, params=payload)
         result = response.json()
         return  float(result["result"])
-
-my_convert = {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560"
-  }
-print(get_convert_sum(my_convert))
-print(type(get_convert_sum(my_convert)))
